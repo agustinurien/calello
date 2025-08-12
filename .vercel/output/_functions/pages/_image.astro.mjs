a34ad0e977c3725178550edbb6d4f7480f9f83c9
Agustin Urien <agus.urien2@gmail.com>
2025-08-12 14:57:04 -0300

    plis basat de ese error

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Cn1JRPDd.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+
 
 export default defineConfig({
-    output: 'server',         // Activa runtime serverless (SSR)
-    adapter: vercel(),        // Adaptador Vercel Serverless
     integrations: [react()],
 });
